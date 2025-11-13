82a6aadd33a22a8cebf44355a05a5f84ade1ed7d
TingaTalk Admin <tingatalkofficial@gmail.com>
2025-11-13 18:39:38 +0530

    Update logo area to fill full width with centered logo

--- src/components/Sidebar/Logo.tsx
+++ src/components/Sidebar/Logo.tsx
@@ -3,9 +3,9 @@ import logoImg from '../../Assets/logo.png';
 
 const Logo: React.FC = () => {
   return (
-    <div className="flex w-full pt-0 pr-[8px] pb-0 pl-[8px] flex-col gap-[10px] items-start">
-      <div className="w-[125px] h-[38px] flex items-center bg-[#1e4841] rounded-[8px] px-[12px]">
-        <img src={logoImg} alt="TingaTalk" className="h-full w-auto object-contain" />
+    <div className="flex w-full -mx-[16px] -mt-[22px] mb-[10px]">
+      <div className="w-full bg-[#1e4841] rounded-[12px] py-[16px] px-[16px] flex items-center justify-center">
+        <img src={logoImg} alt="TingaTalk" className="h-[32px] w-auto object-contain" />
       </div>
     </div>
   );
