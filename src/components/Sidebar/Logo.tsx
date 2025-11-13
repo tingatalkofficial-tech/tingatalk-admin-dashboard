7f1752322d38b1cbaccf1557045c96575a5286dd
TingaTalk Admin <tingatalkofficial@gmail.com>
2025-11-13 18:35:04 +0530

    Add dark background to logo for better visibility

--- src/components/Sidebar/Logo.tsx
+++ src/components/Sidebar/Logo.tsx
@@ -4,8 +4,8 @@ import logoImg from '../../Assets/logo.png';
 const Logo: React.FC = () => {
   return (
     <div className="flex w-full pt-0 pr-[8px] pb-0 pl-[8px] flex-col gap-[10px] items-start">
-      <div className="w-[125px] h-[38px] flex items-center">
-        <img src={logoImg} alt="COINEST" className="h-full w-auto object-contain" />
+      <div className="w-[125px] h-[38px] flex items-center bg-[#1e4841] rounded-[8px] px-[12px]">
+        <img src={logoImg} alt="TingaTalk" className="h-full w-auto object-contain" />
       </div>
     </div>
   );
